8a54fdef6f5d072933aad4c596041ddf7fec61ab
Hilton Meyer <development@hiltonmeyer.com>
2021-05-12 14:53:01 +0300

    feat(setup): Create tests folder

--- src/tests/HelloWorld.spec.ts
+++ src/tests/HelloWorld.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import HelloWorld from './HelloWorld.vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
 describe('HelloWorld', () => {
   it('should display header text', () => {
