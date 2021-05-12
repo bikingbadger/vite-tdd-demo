1d2a85296142fa57ed87e3be16e64e4fb7f82545
Hilton Meyer <development@hiltonmeyer.com>
2021-05-12 15:16:23 +0300

    feat(setup): Add vue router

---
+++ src/components/Greeting.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ greeting }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Greeting",
+
+  data() {
+    return {
+      greeting: "Vue and TDD"
+    }
+  }
+}
+</script>
+
